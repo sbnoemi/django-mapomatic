f5c23033e29ed24ee15952f7a6abca30fc7a301a
Noemi Millman <noemi@strayboots.com>
2012-02-04 16:54:36 -0500

    still tweaking config, apparently

--- sample_project/settings.py
+++ sample_project/settings.py
@@ -100,7 +100,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.messages.middleware.MessageMiddleware',
 )
 
-ROOT_URLCONF = 'chargemeup.urls'
+ROOT_URLCONF = 'sample_project.urls'
 
 TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
