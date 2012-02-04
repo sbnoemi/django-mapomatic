ea2ffafa840eecdf207a0b855eeef3b874e18217
Noemi Millman <noemi@strayboots.com>
2012-02-04 18:52:54 -0500

    send map points to our view for display in sample project

--- sample_project/outlets/views.py
+++ sample_project/outlets/views.py
@@ -1,5 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render_to_response
+from mapomatic.models import MapPoint
 
 def index(request):
-    return render_to_response('mapomatic/map.html')
+    map_points = MapPoint.objects.all()
+    return render_to_response('mapomatic/map.html', {'map_points': map_points,})
